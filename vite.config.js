03f511ebaff957f934014fa12c97152f05d606d5
David Wogelius <davidwogelius@gmail.com>
2025-04-18 18:13:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/nuksnuks/' : '/',
   plugins: [react()],
   build: {
     outDir: './dist'
