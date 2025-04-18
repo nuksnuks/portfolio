166c8f9b0bf2eee3c5f78d6d7ce5607e1825546d
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2025-04-18 14:20:26 +0000

    Deploying to gh-pages from @ nuksnuks/portfolio@82f7d0c6e3d72d32098572b27ac791d2fb13fb5e 🚀

--- src/components/Model.jsx
+++
@@ -1,7 +0,0 @@
-import { useRef, useEffect } from 'react'
-import { useGLTF } from '@react-three/drei'
-
-export function Model({ url, ...props }) {
-    const { scene } = useGLTF(url)
-    return <primitive object={scene} {...props} />
-}
