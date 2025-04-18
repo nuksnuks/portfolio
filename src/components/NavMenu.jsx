cce95fedfa82fa7bbf18c6adfcf81757650eb0ea
David Wogelius <davidwogelius@gmail.com>
2025-04-18 19:23:29 +0200

    Update NavMenu.jsx

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const NavMenu = () => {
     
   return (
     <nav>
       <menu>
-          <a href="/">Home</a>
-          <a href="projects">Projects</a>
+          <Link to="/">Home</Link>
+          <Link to="projects">Projects</Link>
       </menu>
     </nav>
   )
