1035f60b41a4f15cd649f80b4838009528de0d7f
David Wogelius <davidwogelius@gmail.com>
2025-04-18 15:49:43 +0200

    Update GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
