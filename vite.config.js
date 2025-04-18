b257d38ffae8c471eb6226288995ac7df3c63ad7
David Wogelius <davidwogelius@gmail.com>
2025-04-18 15:09:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace with your GitHub username
+  base: '/nuksnuks/', // Replace with your GitHub username
   build: {
     outDir: 'dist'
   }
