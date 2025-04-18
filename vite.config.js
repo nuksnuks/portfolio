7630b48a243daf395e1d796371dba3cfb7af46a0
David Wogelius <davidwogelius@gmail.com>
2025-04-18 15:52:18 +0200

    Update build configuration

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsInlineLimit: 0
   }
 })
