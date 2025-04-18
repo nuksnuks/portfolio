e16b594532866810b1ed2e9bc82b3c7f5f740759
David Wogelius <davidwogelius@gmail.com>
2025-04-18 16:22:34 +0200

    Update build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsInlineLimit: 0
+    outDir: 'dist'
   }
 })
